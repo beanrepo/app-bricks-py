ae740c815cf047749ae356a710ab937f47611c35
Mirko Curtolo <mirko.curtolo86@gmail.com>
2025-12-19 13:49:28 +0100

    Add a Telegram bot brick

---
+++ src/arduino/app_bricks/telegram_bot/__init__.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
+#
+# SPDX-License-Identifier: MPL-2.0
+
+from .telegram_bot import TelegramBot
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+
+__all__ = ["TelegramBot", "Update", "Application", "CommandHandler", "MessageHandler", "filters"]
---
+++ src/arduino/app_bricks/telegram_bot/telegram_bot.py
@@ -0,0 +1,39 @@
+# SPDX-FileCopyrightText: Copyright (C) ARDUINO SRL (http://www.arduino.cc)
+#
+# SPDX-License-Identifier: MPL-2.0
+
+import os
+from arduino.app_utils import brick, Logger
+from telegram import Update
+from telegram.ext import Application, CommandHandler, MessageHandler, filters
+
+logger = Logger("TelegramBot")
+
+@brick
+class TelegramBot:
+    """A brick to manage Telegram Bot interactions."""
+
+    def __init__(self, token: str = None):
+        """Initialize the bot with a token from arg or environment variable."""
+        self.token = token or os.getenv("TELEGRAM_BOT_TOKEN")
+        if not self.token:
+            raise ValueError("Telegram BOT_TOKEN must be provided or set as environment variable")
+
+        self.application = Application.builder().token(self.token).build()
+
+    def add_command(self, command: str, callback):
+        """Register a slash command (e.g., /start)."""
+        self.application.add_handler(CommandHandler(command, callback))
+
+    def on_text(self, callback):
+        """Register a handler for text messages."""
+        self.application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, callback))
+
+    def on_photo(self, callback):
+        """Register a handler for photo messages."""
+        self.application.add_handler(MessageHandler(filters.PHOTO & ~filters.COMMAND, callback))
+
+    def run(self):
+        """Start the Telegram polling loop."""
+        logger.info("Telegram Bot starting...")
+        self.application.run_polling(allowed_updates=Update.ALL_TYPES)
